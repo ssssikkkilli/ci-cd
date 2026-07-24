9e7ca037303b38880d2aa857073d3fa7b2d213b3
ssssikkkilli <li1783075453@qq.com>
2026-07-24 02:41:15 +0000

    Docker

---
+++ src/main/java/org/example/cicd/controller/MessageController.java
@@ -0,0 +1,13 @@
+package org.example.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/message")
+    public String Hell0(){
+        return "hello";
+    }
+}
